ccb6478b172dee29e47f1ac83d004ca2c6cc8248
CryptoSquad <demasmamas@gmail.com>
2023-04-03 01:37:40 +0300

    Add error message on input

--- src/utils/Validators.ts
+++ src/utils/Validators.ts
@@ -1,52 +1,73 @@
 
-
-
-export const isEmail = (value: string) => {
-  let regex = /[\da-z-]+@+[a-z]+.+[a-z]/i
-  if(regex.test(value))
-    return true
-  else
-    return false
+const checkMinLength = (value: string, min: number): string | void => {
+  if(value.length < min) {
+    return `Длина должна быть больше ${min}`
+  }
+}
+const checkMaxLength = (value: string, max: number): string | void  => {
+  if(value.length > max) {
+    return `Длина должна быть меньше ${max}`
+  }
 }
 
-export const isPassword = (value: string) => {
-  if(value.length > 40 || value.length < 8)
-    return false
+
+export const isEmail = (value: string): string | void => {
+  let regex = /[\da-z-]+@+[a-z]+.+[a-z]/i
+  if(!regex.test(value))
+    return "Введите имейл"
+}
+
+export const isPassword = (value: string): string | void => {
+  const checkMaxLengthResult =  checkMaxLength(value, 40);
+  const checkMinLengthResult =  checkMinLength(value, 8);
+  if(checkMaxLengthResult)
+    return checkMaxLengthResult
+  if(checkMinLengthResult)
+    return checkMinLengthResult
+
   let hasDigit = /\d/
   let hasUpperCaseChar = /[A-Z]/
-  if(hasDigit.test(value) && hasUpperCaseChar.test(value))
-    return true
-  else
-    return false
+  if(!hasDigit.test(value))
+    return "Должна быть хотя бы одна цифра"
+  if(!hasUpperCaseChar.test(value))
+    return "Должна быть хотя бы одна заглавная буква"
 }
 
-export const isTel = (value: string) => {
-  if(value.length > 15 || value.length < 10)
-    return false
+export const isTel = (value: string): string | void => {
+  const checkMaxLengthResult = checkMaxLength(value, 15);
+  const checkMinLengthResult = checkMinLength(value, 10);
+  if(checkMaxLengthResult)
+    return checkMaxLengthResult
+  if(checkMinLengthResult)
+    return checkMinLengthResult
+
   let regex = /^[+\d]+[\d]+\d$/
-  if(regex.test(value))
-    return true
-  else
-    return false
+  if(!regex.test(value))
+    return "Введите телефон"
 }
 
-export const isLogin = (value: string) => {
-  if(value.length > 20 || value.length < 3)
-    return false
+export const isLogin = (value: string): string | void => {
+  const checkMaxLengthResult =  checkMaxLength(value, 20);
+  const checkMinLengthResult =  checkMinLength(value, 3);
+  if(checkMaxLengthResult)
+    return checkMaxLengthResult
+  if(checkMinLengthResult)
+    return checkMinLengthResult
+
   let regex = /^[+\d]+[\d]+\d$/
-  if(regex.test(value))
-    return true
-  else
-    return false
+  if(!regex.test(value))
+   return "Не соответствует шаблону"
 }
 
-export const isName = (value: string) => {
+export const isName = (value: string): string | void => {
 
   let hasSpace = /\s/
   let hasDigit = /\d/
   let regex = /^[A-ZА-Я][a-zA-Zа-яА-Я-]*/u
-  if(!hasSpace.test(value) && !hasDigit.test(value) && regex.test(value))
-    return true
-  else
-    return false
+  if(hasSpace.test(value))
+    return "Не должно быть пробелов"
+  if(hasDigit.test(value))
+    return "Не должно быть цифр"
+  if(regex.test(value))
+    return "Не соответствует шаблону"
 }
